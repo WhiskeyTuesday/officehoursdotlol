f86677a13be59e7b4a33b04702327dbf2f722703
Elija Sorensen <eli.r.sorensen@gmail.com>
2024-06-29 18:18:52 -0700

    mobile qrcode fix

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script>
+  import { onMount } from 'svelte';
   import qr from 'qrcode-generator';
   import shuffle from 'lodash.shuffle';
 
@@ -7,6 +8,11 @@
   selfqr.make();
 
   const pages = shuffle(['nyc', 'sfo', 'yvr']);
+
+  let windowWidth;
+  onMount(() => {
+    windowWidth = window.innerWidth;
+  });
 </script>
 
 <h1>office hours dot lol</h1>
@@ -17,12 +23,23 @@
   {/each}
 </ul>
 
-<p>If I have the wrong contact person listed on a page or bad information or you want to tell me something interesting or you want me to add a city or you want me to add a warning on a page or whatever let me know <a href="https://www.twitter.com/whiskeytuesday">@whiskeytuesday</a></p>
+<p>If I have the wrong contact person listed on a page or bad information or you want to tell me something interesting or you want me to add a city or you want me to add a warning on a page or give me a bug report or whatever let me know <a href="https://www.twitter.com/whiskeytuesday">@whiskeytuesday</a></p>
 
-<h4>This page:</h4>
-{#if selfqr}
-  <pre>{selfqr.createASCII(1)}</pre>
-{/if}
+<div class="qrcode">
+  {#if selfqr}
+    <h4>This page:</h4>
+    <!-- if on mobile -->
+    {#if windowWidth < 600}
+      {@html selfqr.createSvgTag({ scalable: true, margin: 0 })}
+    {:else}
+      <pre>{selfqr.createASCII(2)}</pre>
+    {/if}
+  {/if}
+</div>
 
-<style global>
+<style>
+  .qrcode {
+    width: 100%;
+    max-width: 60%;
+  }
 </style>
