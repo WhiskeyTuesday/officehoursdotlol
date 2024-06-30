f40818190cf2db3936a8a4243502702fcb729edb
Elija Sorensen <eli.r.sorensen@gmail.com>
2024-06-29 18:45:12 -0700

    deepfates didn't correct me must be right

--- src/routes/sfo/+page.svelte
+++ src/routes/sfo/+page.svelte
@@ -1,5 +1,5 @@
 <h1>sfo</h1>
-<p>Tuesdays at 6pm at Zeitgeist as far as I know</p>
+<p>Tuesdays at 6pm at Zeitgeist</p>
 <p>contact <a href="https://twitter.com/_deepfates">@_deepfates</a> for more info</p>
 
 <h3><a href="/">home</a></h3>
