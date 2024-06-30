8ac1dfc8e01766ebd3893d44179306e7d290df96
Elija Sorensen <eli.r.sorensen@gmail.com>
2024-06-29 19:54:10 -0700

    I'm a professional writer

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,7 +11,7 @@
   {/each}
 </ul>
 
-<p>If I have the wrong contact person listed on a page or bad information or you want to tell me something interesting or you want me to add a city or you want me to add a warning on a page or give me a bug report or whatever let me know <a href="https://www.twitter.com/whiskeytuesday">@whiskeytuesday</a></p>
+<p>If I have the wrong contact person listed on a page or bad information or you want to tell me something interesting or you want me to add a city or you want me to add a warning on a page or you want to give me a bug report or whatever let me know <a href="https://www.twitter.com/whiskeytuesday">@whiskeytuesday</a></p>
 
 <p>The QR codes on each page link back to themselves so you can help a friend bookmark the page for their local event or whatever. They were super cool ASCII QR codes but they refuse to render correctly on mobile so they're boring svgs now. I'm not salty about it at all.</p>
 
