2ee017b001ba35b3f632f2eab7dd4915d07c589a
Elija Sorensen <eli.r.sorensen@gmail.com>
2024-07-10 16:16:44 -0700

    update yvr location and details

--- src/routes/yvr/+page.svelte
+++ src/routes/yvr/+page.svelte
@@ -2,13 +2,24 @@
 
 <h3>Wednesdays at 6pm</h3>
 
-<div class="warn">
-  <p>Location TBD. We went to the cambie last time but it's not ideal.</p>
-  <p>If you have a suggestion of a good summer spot with large outdoor tables dm @whiskeytuesday</p>
+<div class="info-green">
+  <p>We're at The Lido (near Broadway and Kingsway) this week.</p>
+  <p>
+    It might not be ideal if more people start coming,
+    the tables are pretty small.
+  </p>
+  <p>If you have a suggestion of a good summer spot with large outdoor tables dm
+    <a href="https://twitter.com/whiskeytuesday">@whiskeytuesday</a>.
+  </p>
 </div>
 
-<p>contact <a href="https://twitter.com/whiskeytuesday">@whiskeytuesday</a> for more info</p>
+<p>contact
+  <a href="https://twitter.com/whiskeytuesday">@whiskeytuesday</a>
+  for more info.
+</p>
 
 <div class="info-blue">
-  <p>Look for the table with the giant rubber duck</p>
+  <p>
+    Look for the table with the giant rubber duck &#x1F986; &#x1F986; &#x1F986;
+  </p>
 </div>
