fd1d19e73509b4650f71e7d9903ad33bfd3f2fa6
Elija Sorensen <eli.r.sorensen@gmail.com>
2024-07-16 15:54:11 -0700

    adds phi

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script>
   import shuffle from 'lodash.shuffle';
-  const pages = shuffle(['nyc', 'sfo', 'yvr', 'chi']);
+  const pages = shuffle(['nyc', 'sfo', 'yvr', 'chi', 'phi']);
 </script>
 
 <h1>office hours dot lol</h1>
@@ -11,8 +11,21 @@
   {/each}
 </ul>
 
-<p>If I have the wrong contact person listed on a page or bad information or you want to tell me something interesting or you want me to add a city or you want me to add a warning on a page or you want to give me a bug report or whatever let me know <a href="https://www.twitter.com/whiskeytuesday">@whiskeytuesday</a></p>
+<p>
+  If I have the wrong contact person listed on a page or bad
+  information or you want to tell me something interesting
+  or you want me to add a city or you want me to add a
+  warning on a page or you want to give me a bug report or
+  whatever let me know
+  <a href="https://www.twitter.com/whiskeytuesday">@whiskeytuesday</a>
+</p>
 
-<p>The QR codes on each page link back to themselves so you can help a friend bookmark the page for their local event or whatever. They were super cool ASCII QR codes but they refuse to render correctly on mobile so they're boring svgs now. I'm not salty about it at all.</p>
+<p>
+  The QR codes on each page link back to themselves so you
+  can help a friend bookmark the page for their local event
+  or whatever. They were super cool ASCII QR codes but they
+  refuse to render correctly on mobile so they're boring
+  svgs now. I'm not salty about it at all.
+</p>
 
 <p>Dark mode coming soon™</p>
