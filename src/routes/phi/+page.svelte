6b96464591bfc0c4ef3a3897ffc2b018a51d1c85
Elija Sorensen <eli.r.sorensen@gmail.com>
2024-07-16 16:01:24 -0700

    actuall add the page though idiot

---
+++ src/routes/phi/+page.svelte
@@ -0,0 +1,26 @@
+<h1>phi</h1>
+
+<h3><em>Occasional</em> Tuesdays at 6pm</h3>
+
+<div class="info-green">
+  <p><em>Occasional</em> Tuesdays at 6pm at Bonner's Irish Pub</p>
+</div>
+
+<div class="warn">
+  <p>Not guaranteed to be weekly until further notice.</p>
+  <p>
+    Check <a href="https://twitter.com/FeralPHunter">@FeralPHunter</a>
+    for updates.
+  </p>
+  <p>
+    If you want this to become a weekly thing show up and tell
+    Pawg how great it is that he's putting in the effort
+    to host the event and making it happen and how overwhelmingly
+    sexually attractive you find him. That ought to do it.
+  </p>
+</div>
+
+<p>Contact
+  <a href="https://twitter.com/FeralPHunter">@FeralPHunter</a> with any
+  questions you may have.
+</p>
