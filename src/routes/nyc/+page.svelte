873e31687e1b6a0e38ce63d5ba4f0849db95f21e
Elija Sorensen <WhiskeyTuesday@users.noreply.github.com>
2025-02-11 11:19:27 -0800

    Update nyc

--- src/routes/nyc/+page.svelte
+++ src/routes/nyc/+page.svelte
@@ -1,3 +1,3 @@
 <h1>nyc</h1>
-<p>Tuesdays at 6pm at McSorely's</p>
+<p>Tuesdays at 6pm at McSorely's, except when it's somewhere else; check Twitter 🤷‍♂️</p>
 <p>contact <a href="https://twitter.com/inflammateomnia">@inflammateomnia</a> for more info</p>
