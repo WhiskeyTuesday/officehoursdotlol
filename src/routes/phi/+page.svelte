d8ab5df4a5ba345e4d86e6732abc00cf12fcd8d6
Elija Sorensen <eli.r.sorensen@gmail.com>
2025-02-18 13:26:24 -0800

    update phi (now weekly!)

--- src/routes/phi/+page.svelte
+++ src/routes/phi/+page.svelte
@@ -3,24 +3,24 @@
 <h3><em>Occasional</em> Tuesdays at 6pm</h3>
 
 <div class="info-green">
-  <p><em>Occasional</em> Tuesdays at 6pm at Bonner's Irish Pub</p>
+  <p>Tuesdays at 6:30 pm at Bonner's Irish Pub</p>
 </div>
 
-<div class="warn">
-  <p>Not guaranteed to be weekly until further notice.</p>
+<div class="info-blue">
   <p>
     Check <a href="https://twitter.com/FeralPHunter">@FeralPHunter</a>
     for updates.
   </p>
-  <p>
+  <p><s>
     If you want this to become a weekly thing show up and tell
     Pawg how great it is that he's putting in the effort
     to host the event and making it happen and how overwhelmingly
     sexually attractive you find him. That ought to do it.
+  </s></p>
+</div>
+<div class="info-green">
+  <p>
+    UPDATE: It's an officially weekly event now. Remember to
+    sexually harass your host as (evidently) he enjoys it.
   </p>
 </div>
-
-<p>Contact
-  <a href="https://twitter.com/FeralPHunter">@FeralPHunter</a> with any
-  questions you may have.
-</p>
