a1bd481884815d3a93b50f39df9249135aacc16e
Elija Sorensen <eli.r.sorensen@gmail.com>
2025-06-25 10:27:44 -0700

    update nyc, gently mock them

--- src/routes/nyc/+page.svelte
+++ src/routes/nyc/+page.svelte
@@ -1,6 +1,14 @@
 <h1>nyc</h1>
 <h2>New York, New York</h2>
 
-<p>Tuesdays at 6pm at McSorely's, except when it's somewhere else; check Twitter 🤷‍♂️</p>
+<p>Tuesdays at 6pm at McCarren Park in Williamsburg if the weather is nice</p>
+
+<div class="info-blue">
+  <p>
+    If the weather is bad it's at "a bar nearby",
+    I think the nyc crowd prefers a bit of illegibility.
+    Get into a cool-kids groupchat or something I guess.
+  </p>
+</div>
 
 <p>contact <a href="https://twitter.com/inflammateomnia">@inflammateomnia</a> for more info</p>
