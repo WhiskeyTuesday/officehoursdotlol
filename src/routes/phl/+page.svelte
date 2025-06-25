6be9b0b1fffe26aa1ff4d7618bf01f91ffe57633
Elija Sorensen <eli.r.sorensen@gmail.com>
2025-06-25 10:23:17 -0700

    unmeme phl

--- src/routes/phl/+page.svelte
+++ src/routes/phl/+page.svelte
@@ -12,16 +12,4 @@
     Check <a href="https://twitter.com/FeralPHunter">@FeralPHunter</a>
     for updates.
   </p>
-  <p><s>
-    If you want this to become a weekly thing show up and tell
-    Pawg how great it is that he's putting in the effort
-    to host the event and making it happen and how overwhelmingly
-    sexually attractive you find him. That ought to do it.
-  </s></p>
-</div>
-<div class="info-green">
-  <p>
-    UPDATE: It's an officially weekly event now. Remember to
-    sexually harass your host as (evidently) he enjoys it.
-  </p>
 </div>
