a188cca0d58f6ffcf00c8a5d0b03108015bb5b85
Elija Sorensen <eli.r.sorensen@gmail.com>
2025-06-25 10:24:25 -0700

    minor fix

--- src/routes/ord/+page.svelte
+++ src/routes/ord/+page.svelte
@@ -6,7 +6,7 @@
 <div class="alert">
   <p>@crispywicks left Chicago in July 2024 and no longer runs office hours chi</p>
   <p>
-    If you want to take the responsibility of organizing chi office hours
+    If you want to take the responsibility of organizing ord office hours
     contact <a href="https://twitter.com/whiskeytuesday">@whiskeytuesday</a>
   </p>
 </div>
