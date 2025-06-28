7f12abe58faae7a63825b754e500f9513ff917d3
Elija Sorensen <eli.r.sorensen@gmail.com>
2025-06-28 13:03:31 -0700

    better tips

--- src/routes/gettingstarted/+page.svelte
+++ src/routes/gettingstarted/+page.svelte
@@ -3,7 +3,10 @@
 
 <ul>
   <li>pick a consistent time and place if you can</li>
-  <li>have a "totem" item that you can use to identify your table so introverts don't have to ask around. "Is this the twitter table?" Mortifying. Most branches use a rubber duck, but you can use whatever you want.</li>
+  <li>It's still office hours if no one shows up. It's still office hours if only one person shows up.</li>
+  <li>Have a "totem" item that you can use to identify your table so introverts don't have to ask around. "Is this the twitter table?" Mortifying. Most branches use a rubber duck, but you can use whatever you want.</li>
+  <li>If you do have a totem please add it to your city's page via a pull request or by pinging <a href="https://www.twitter.com/whiskeytuesday">@whiskeytuesday</a></li>
+  <li>Anyone can bring a totem item, anyone can make a post about an office hours new or existing, everyone is the host</li>
   <li>You can do weekly, bi-weekly, monthly, whatever you want. If you're not going to be weekly consider doing "first and third tuesdays" or whatever and not "every second"</li>
   <li>Submit a PR to this website's repo or ping <a href="https://www.twitter.com/whiskeytuesday">@whiskeytuesday</a> to get your office hours added</li>
 </ul>
