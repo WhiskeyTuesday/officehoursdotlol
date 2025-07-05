8b35b11c4bde388079b7e1e3446b8bd4bccbaef1
Elija Sorensen <eli.r.sorensen@gmail.com>
2025-07-05 13:25:53 -0700

    adds austin

---
+++ src/routes/aus/+page.svelte
@@ -0,0 +1,21 @@
+<h1>aus</h1>
+<h2>Austin, Texas</h2>
+
+<h3>Wednesdays at 6:30pm</h3>
+
+<div class="info-green">
+  <p>Cosmic Coffee + Beer Garden Saltillo Plaza</p>
+</div>
+<div class="warn">
+  <p>Cosmic Saltillo on East 4th, not Cosmic on Pickle</p>
+</div>
+
+<p>
+  contact
+  <a href="https://twitter.com/wigglybone">@wigglybone</a>
+  for more info.
+</p>
+
+<div class="info-blue">
+    Look for a table with a giant rubber duck &#x1F986; &#x1F986; &#x1F986;
+</div>
