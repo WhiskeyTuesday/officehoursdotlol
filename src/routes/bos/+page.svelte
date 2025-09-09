a072c7794a6bb9d71cc689e97b9555350dd72510
superb-owl <109551448+superb-owl@users.noreply.github.com>
2025-09-09 13:54:48 -0400

    Update +page.svelte

--- src/routes/bos/+page.svelte
+++ src/routes/bos/+page.svelte
@@ -6,12 +6,15 @@
 <div class="info-blue">
   <p>
     at <a href="https://maps.app.goo.gl/bqKD3NoYm6hyMNAd7">The Cellar</a>
-    (which is down the stairs under the wine store).
+    (which is down the stairs under the wine store). Look for a rubber duck.
   </p>
 </div>
 
 <p>
-  contact
-  <a href="https://twitter.com/quotidiania">@quotidiania</a>
-  for more info.
+  Time and location is usually consistent, but may vary. We do field trips sometimes.
+</p>
+<p>
+  Contact
+  <a href="https://twitter.com/owlthatissuperb">@owlthatissuperb</a>
+  or join the Boston TPOT groupchat for more info.
 </p>
