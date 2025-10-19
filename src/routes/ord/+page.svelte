bfc10c32aea7381326330677cd8648639a39a451
aaron <ajwills@umich.edu>
2025-10-19 16:00:20 -0500

    ord office hours are back

--- src/routes/ord/+page.svelte
+++ src/routes/ord/+page.svelte
@@ -1,12 +1,15 @@
 <h1>ord</h1>
 <h2>Chicago, Illinois</h2>
 
-<p>Formerly Thursdays at 6pm at The Abe Lincoln statue in Lincoln Park</p>
+<p>2nd tuesday of the month at 6pm</p>
 
-<div class="alert">
-  <p>@crispywicks left Chicago in July 2024 and no longer runs office hours chi</p>
+<div class="info-green">
+  <p>bourgeois pig cafe @ 738 w fullerton ave</p>
   <p>
-    If you want to take the responsibility of organizing ord office hours
-    contact <a href="https://twitter.com/whiskeytuesday">@whiskeytuesday</a>
+    contact <a href="https://twitter.com/lobothoughts">@lobothoughts</a> for more info
   </p>
+</div>
+
+<div class="info-blue">
+    Look for a table with a crochet plant.
 </div>
