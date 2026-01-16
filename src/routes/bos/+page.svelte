0331004b967f746bd2bb51a33dbfef4740308ee4
superb-owl <109551448+superb-owl@users.noreply.github.com>
2026-01-15 17:23:57 -0800

    Update +page.svelte

--- src/routes/bos/+page.svelte
+++ src/routes/bos/+page.svelte
@@ -1,20 +1,21 @@
 <h1>bos</h1>
 <h2>Boston, Massachusetts</h2>
 
-<h3>Thursdays at 6:30pm</h3>
+<h3>Mondays at 7:00pm</h3>
 
 <div class="info-blue">
   <p>
-    at <a href="https://maps.app.goo.gl/bqKD3NoYm6hyMNAd7">The Cellar</a>
-    (which is down the stairs under the wine store). Look for a rubber duck.
+    usually at <a href="https://maps.app.goo.gl/V6N6FCz7oCPncatcA">Lamplighter Brewery</a>.
+    Look for a rubber duck, or failing that, a bunch of nerds.
   </p>
 </div>
 
 <p>
-  Time and location is usually consistent, but may vary. We do field trips sometimes.
+  <b>Time and location may vary!<b> We do field trips sometimes. Get in with us below if you're planning to come.
 </p>
 <p>
-  Contact
-  <a href="https://twitter.com/owlthatissuperb">@owlthatissuperb</a>
-  or join the Boston TPOT groupchat for more info.
+  Contact Max
+  <a href="https://twitter.com/owlthatissuperb">on Twitter</a>
+  or <a href="mailto:max@superb-owl.link">via email</a>
+  to verify hours or to join the Boston TPOT groupchat.
 </p>
