a0cd262d11f5e695351f2386b9bc1763fab3eb4e
Bijan Haney <bijanhaney@gmail.com>
2026-02-10 21:25:44 -0500

    Update +page.svelte
    Winter location

--- src/routes/nyc/+page.svelte
+++ src/routes/nyc/+page.svelte
@@ -1,13 +1,11 @@
 <h1>nyc</h1>
 <h2>New York, New York</h2>
 
-<p>Tuesdays at 6pm at McCarren Park in Williamsburg if the weather is nice</p>
+<p>Tuesdays at 6:30pm at d.b.a in the LES when its cold, and at McCarren Park in Williamsburg if the weather is nice</p>
 
 <div class="info-blue">
   <p>
-    If the weather is bad it's at "a bar nearby",
-    I think the nyc crowd prefers a bit of illegibility.
-    Get into a cool-kids groupchat or something I guess.
+    Look for a rubber duck. Because it changes depending on the weather, get into a cool-kids groupchat or something I guess to known exactly where it will be. 
   </p>
 </div>
 
