853bc610bb2c2c33a8893c115025f08954abc5f6
PavedWalden <pavedwalden@gmail.com>
2026-02-16 14:35:08 -0800

    add PDX office hours

---
+++ src/routes/pdx/+page.svelte
@@ -0,0 +1,16 @@
+<h1>pdx</h1>
+<h2>Portland, Oregon</h2>
+
+<h3>Tuesdays at 6:00pm</h3>
+
+<div class="info-green">
+  <p>
+    Horse Brass Pub, 4534 SE Belmont St
+  </p>
+</div>
+
+<hr />
+
+<p>
+  Contact <a href="https://twitter.com/pavedwalden">@pavedwalden</a> for more info.
+</p>
