06f5d03f9feaea1aca0ed310be9e2bb0b06a97a4
Elija Sorensen <elija@hey.com>
2026-06-23 18:40:32 -0700

    i know what year it is shut up no you shut up

--- src/routes/msy/+page.svelte
+++ src/routes/msy/+page.svelte
@@ -1,10 +1,10 @@
 <h1>msy</h1>
 <h2>New Orleans, LA</h2>
 
-<h3>Was held twice before Mardi Gras 2025</h3>
+<h3>Was held twice before Mardi Gras 2026</h3>
 
 <div class="info-blue">
-  <p>Perhaps to return for a few weeks in 2026, we'll see</p>
+  <p>Perhaps to return for a few weeks in 2027, we'll see</p>
   <p>
     If you'd like to start a permanent msy office hours contact
     <a href="https://twitter.com/whiskeytuesday">@whiskeytuesday</a>
