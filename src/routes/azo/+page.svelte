79c82254a2c06554b4bbf184b5cd898bd3d7e274
Aaron Ulbricht <aaron.ulbricht@ucsf.edu>
2026-08-02 22:57:14 -0400

    add Kalamazoo

---
+++ src/routes/azo/+page.svelte
@@ -0,0 +1,24 @@
+<h1>azo</h1>
+<h2>Kalamazoo, Michigan</h2>
+
+<p>
+  Fourth Saturday of the month, lunch at 2PM at
+  The Backyard of Kalamazoo
+</p>
+
+<div class="info-green">
+  <a href="https://www.backyardkzoo.com/">
+    10441 Stadium Dr, Kalamazoo, MI 49009
+  </a>
+</div>
+
+<div class="info-blue">
+  <p>
+    Contact
+    <a href="https://twitter.com/AaronEstel">@AaronEstel</a>
+    for more information.
+  </p>
+</div>
+
+<p>Totem: look for the large golden Rubber Ducky</p>
+
