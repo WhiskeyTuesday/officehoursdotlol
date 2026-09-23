ff164d8f65b44dd3ce64fe4551a3a0ecd89f9a11
Dan Tasse <105866+dantasse@users.noreply.github.com>
2026-09-23 14:18:00 -0400

    move pit to last tuesday, add Sean (#26)

--- src/routes/pit/+page.svelte
+++ src/routes/pit/+page.svelte
@@ -1,15 +1,16 @@
 <h1>pit</h1>
 <h2>Pittsburgh, Pennsylvania</h2>
 
-<h3>4th Tuesdays at 7pm</h3>
+<h3>Last Tuesdays at 7pm</h3>
 
 <div class="info-green">
-  <p>4th Tuesday of the month at 7pm at The Porch in Oakland</p>
+  <p>Last Tuesday of the month at 7pm at The Porch in Oakland</p>
 </div>
 
 <div class="info-blue">
   <p>
-    Ask <a href="https://twitter.com/sedatesnail">@sedatesnail</a>
+    Ask <a href="https://x.com/sedatesnail">@sedatesnail</a>
+    or <a href="https://x.com/TheUnjournaling">@TheUnjournaling</a>
     for updates.
   </p>
 </div>
